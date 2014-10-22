b6fc7e3f081d4374f0e886856f5bd543e6817bde
mfabros <melissa.fabros@gmail.com>
2014-10-21 20:26:05 -0700

    fixed random weather file, split function, improve formatting for weather report

--- week4/more_weather.py
+++ week4/more_weather.py
@@ -6,6 +6,8 @@ import requests
 # print ny[1]
 
 BASE_URL_Forecast_10day = 'http://api.wunderground.com/api/63577728b0e9ae1f/forecast10day/q/'
+source_places = [('New York', 'NY'), ('San Francisco', 'CA'), ('Seattle', 'WA'), ('Houston', 'TX'), ]
+
 
 	
 def get_api_url_forecast(state, city):
@@ -22,24 +24,26 @@ def forecast(state, city):
 
 	for day in days:
 	#loops through data for specific city to pull day and forecast, with added feature to let you know if it's raining'''
-	    print city, "--", day['title'], ": ", day['fcttext']
+	    return "In {} on {}, the weather will be {}".format(city, day['title'], day['fcttext'])
 	    icon = day['icon'] 
 	    if "rain" in icon:
-	    	print "\n", icon, " bring an umbrella!"
+	    	return "it may rain on {}, don't forget to bring an umbrella".format(day['title'])
+
+def random_location():
+#selects a random state-city pair and splits tuple into separate variables of city and state 
+	
+	random_place = random.choice(source_places)
+	state = random_place[1]
+	city = random_place[0]
+	return (state, city)
+	
 
 
+def random_weather_forecast(state, city):
+	'''applies random city choice to the forecast functions
+	to generate the weather report for the randomly chosen city'''
+	the_weather = forecast(state, city)
+	print the_weather
 
-#def random_weather_forecast(state, city):
-'''applies random city choice to the forecast functions
-to generate the weather report for the randomly chosen city'''
-source_places = [('New York', 'NY'), ('San Francisco', 'CA'), ('Seattle', 'WA'), ('Houston', 'TX'), ]
-random_place = random.choice(source_places)
-state = random_place[1]
-city = random_place[0]
-#weather_at_location = get_api_url_forecast(state, city)
-the_weather = forecast(state, city)
-print the_weather
-
-
-
-#it works just as is now, but i broke it when i wrapped in in function?
+(state, city) = random_location()
+random_weather_forecast(state, city)
